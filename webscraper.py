b8f3c1005ef06d1166abc338522ddf7e6a41ce2a
jyhuang2005 <jyhuang2005@gmail.com>
2023-05-18 11:24:44 -0400

    randlist

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,26 @@
 from googlesearch import search
 import requests
 from pyhelpers.ops import is_downloadable
+import json
 
-pieces = [
-          "Academic Festival Overture - Johannes Brahms"]
+pieces = []
+
+# Opening JSON file
+with open('pieces.json') as json_file:
+    data = json.load(json_file)['composers']
+    for i in range(len(data)):
+        works = data[i]['works']
+        composer = data[i]['name']
+        for j in range(len(works)):
+            work_name = works[j]['title']
+            if works[j]['popular'] == '1':
+                if work_name[len(work_name) - 1:] == ' ':
+                    pieces.append(work_name + '- ' + composer)
+                else:
+                    pieces.append(work_name + ' - ' + composer)
+
+for i in range(len(pieces)):
+    print(pieces[i])
 
 for i in range(len(pieces)):
     piece = pieces[i]
@@ -23,8 +40,15 @@ for i in range(len(pieces)):
 
     if is_downloadable(rec_link):
         r = requests.get(rec_link, allow_redirects=True)
-        open('recordings/' + str(i) + '. ' + piece + '.mp3', 'wb').write(r.content)
+        open('recordings2/' + str(i) + '. ' + piece + '.mp3', 'wb').write(r.content)
         print('Success: ' + piece)
     else:
         print('Failed: ' + piece)
     # webbrowser.open(rec_link)
+
+if is_downloadable('https://api.openopus.org/work/dump.json'):
+    r = requests.get('https://api.openopus.org/work/dump.json', allow_redirects=True)
+    open('pieces.txt', 'wb').write(r.content)
+    print('Success')
+else:
+    print('Failed')
